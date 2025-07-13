39e4f73228aca1c16ee93888552cd4e190b53c71
Dheraj78788 <147507989+Dheraj78788@users.noreply.github.com>
2025-07-13 19:15:54 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// firebase-config.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBYk_RvoDSHqG4pj4yM0buR3d-9p2hn9Vw",
   authDomain: "voicenote-1.firebaseapp.com",
@@ -18,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
